f67d328be881b7c2273744e8794a438108877a0d
okkokko10 <okko.heinio@gmail.com>
2025-12-20 01:01:04 +0200

    extending the index set. bad idea?

--- OkkokkoLean/MyProgram.lean
+++ OkkokkoLean/MyProgram.lean
@@ -128,21 +128,21 @@ section multi_cover
 
 #check Membership
 
-variable  {X : Type} {F : Type} [SetLike F X]
+variable  {X : Type} {F : Type} [SetLike F X] {c : Cardinal}
 
 #check Subgroup
 
 
 -- todo: other sets than N
 
-structure MultiCover {X : Type} (F : Type) [SetLike F X] where
+
+open scoped Cardinal
+
+-- if c is finite, it's any finite combination
+structure MultiCover {X : Type} (F : Type) (c : Cardinal) [SetLike F X] where
   func (x : X) : ℕ∞
-  possible: ∃(series: ℕ → F), func = fun x ↦ ENat.card {n | x ∈ series n}
+  possible: ∃(ι : Type)(_ : ℵ₀ ≤ #ι → #ι ≤ c)(series: ι → F), func = fun x ↦ ENat.card {n | x ∈ series n}
 
-
--- structure MultiCover {X : Type} (F : Type) (c : Cardinal) [SetLike F X] where
---   func (x : X) : ℕ∞
---   possible: ∃(ι : Type)(_ : Cardinal.mk ι ≤ c)(series: ι → F), func = fun x ↦ ENat.card {n | x ∈ series n}
 
 example : ℕ ≃ ℕ ⊕ ℕ := by exact Equiv.natSumNatEquivNat.symm
 
@@ -155,7 +155,7 @@ example : ℕ ≃ ℕ ⊕ ℕ := by exact Equiv.natSumNatEquivNat.symm
 --   -- func (x : X) : ℕ∞ := ENat.card {n | x ∈ series n}
 -- def MultiCover2.func (f : MultiCover2 F) (x : X) : ℕ∞ := ENat.card {n | x ∈ f.series n}
 
-instance : FunLike (MultiCover F) X ℕ∞ where
+instance : FunLike (MultiCover F c) X ℕ∞ where
   coe := MultiCover.func
   coe_injective' := by
     intro a b fu
@@ -164,7 +164,7 @@ instance : FunLike (MultiCover F) X ℕ∞ where
 
 -- instance : CoeFun (MultiCover F) (fun _ ↦ (X → ℕ∞)) := ⟨MultiCover.func⟩
 
-instance : Membership X (MultiCover F) where
+instance : Membership X (MultiCover F c) where
   mem f x := f x > 0
 
 -- #check type_eq_of_heq
@@ -196,11 +196,11 @@ theorem possible_equiv {ι₁ ι₂ : Type} (e : ι₁ ≃ ι₂) (f : ι₁ →
 --   sorry
 
 
-def alternating' {X : Type} (a b : ℕ → X) : ℕ → X
-  | 0 => a 0
-  | n + 1 => alternating' b (a <| · + 1) n
+-- def alternating' {X : Type} (a b : ℕ → X) : ℕ → X
+--   | 0 => a 0
+--   | n + 1 => alternating' b (a <| · + 1) n
 
-#eval Array.range 10 |>.map <| alternating' ((Array.range 5)[·]!) ((Array.range 5 |>.map (· + 10))[·]!)
+-- #eval Array.range 10 |>.map <| alternating' ((Array.range 5)[·]!) ((Array.range 5 |>.map (· + 10))[·]!)
 
 -- def alternating {X : Type} (a b : ℕ → X) (n : ℕ) : X :=
 --   match (Equiv.natSumNatEquivNat.symm n) with
@@ -216,7 +216,194 @@ def alternating {X : Type} (a b : ℕ → X) (n : ℕ) : X := (Equiv.natSumNatEq
 #check ENat.card
 
 
-instance : Add (MultiCover F) where
+-- the name is just a placeholder
+lemma cw_greater_cardinal {ai : Type} {c : Cardinal} (c_infty : ℵ₀ ≤ c) (ai_c : ℵ₀ ≤ #ai → #ai ≤ c) : #ai ≤ c := by
+        by_cases! aw : #ai < ℵ₀
+        exact le_trans aw.le c_infty
+        exact ai_c aw
+
+instance : Add (MultiCover F c) where
+  add a b := ⟨(a + b), by
+    have ⟨ai,ai_c,as,as_spec⟩ := a.possible
+    have ⟨bi,bi_c,bs,bs_spec⟩ := b.possible
+
+    use ai ⊕ bi
+    refine ⟨?_,?_⟩
+    ·
+      simp only [Cardinal.mk_sum, Cardinal.lift_id]
+      intro infi
+      -- this might could be a lemma
+      have : ℵ₀ ≤ #ai ∨ ℵ₀ ≤ #bi := by exact Cardinal.aleph0_le_add_iff.mp infi
+      have af := fun w ↦ le_trans w (ai_c w)
+      have bf := fun w ↦ le_trans w (bi_c w)
+      have c_infty := this.elim af bf
+
+
+      exact Cardinal.add_le_of_le c_infty (cw_greater_cardinal c_infty ai_c) (cw_greater_cardinal c_infty bi_c)
+
+
+
+    use fun n ↦ n.elim as bs
+    change a.func + b.func = _
+    rw [as_spec,bs_spec]
+    funext x
+    simp only [Pi.add_apply]
+    rw [←ENat.card_sum]
+    apply ENat.card_congr
+    symm
+    trans
+    · apply Equiv.subtypeSum
+    rfl
+
+  ⟩
+
+instance : AddCommSemigroup (MultiCover F c)  where
+  add_assoc a b c := by
+    change a + b + c = ⟨a + (b + c), _⟩
+    ring_nf
+    rfl
+  add_comm a b := by
+    change ⟨a + b, _⟩ = b + a
+    rw [show b + a = ⟨b + a, _⟩ by rfl]
+    simp only [MultiCover.mk.injEq]
+    exact AddCommMagma.add_comm ⇑a ⇑b
+
+-- Surely this already exists.
+class HasEmpty (A : Type*) {B : outParam Type*} [SetLike A B] extends EmptyCollection A where
+  empty_is_empty : SetLike.coe (∅ : A) = ∅
+
+theorem MultiCover.hasEmpty_strict_cardinal [HasEmpty F] (h : ℵ₀ ≤ c) (f : X → ℕ∞) :
+  (∃(ι : Type)(_ : ℵ₀ ≤ #ι → #ι ≤ c)(series: ι → F), f = fun x ↦ ENat.card {n | x ∈ series n})
+  ↔ (∃(series: c.out → F), f = fun x ↦ ENat.card {n | x ∈ series n}) := by
+  constructor
+  ·
+    rw [←Cardinal.mk_out c] at *
+    intro ⟨ι, i_c, ser, ser_spec⟩
+
+
+    have ll : #ι ≤ #c.out := cw_greater_cardinal h i_c
+
+    have emb := ((Cardinal.le_def _ _).mp ll).some
+
+    sorry
+
+  sorry
+
+-- instance [HasEmpty F] : Zero (MultiCover F c) where
+--   zero := ⟨fun x ↦ 0, by
+
+--     use fun i ↦ ∅
+--     funext x
+--     symm
+--     rw [ENat.card_eq_zero_iff_empty]
+--     have : x ∉ (∅ : F) := by
+--       simp [←SetLike.mem_coe]
+--       rw [HasEmpty.empty_is_empty]
+--       exact fun a ↦ a
+--     exact Subtype.isEmpty_of_false fun a ↦ this
+--     ⟩
+
+-- could be changed to embedding for free
+noncomputable def emptyFun (X : Type): Empty → X := by
+  -- there has to be an easier way to access this
+  have : # Empty ≤ # X := by
+    simp only [Cardinal.mk_eq_zero, zero_le]
+  have emb := ((Cardinal.le_def _ _).mp this).some
+  apply emb.toFun
+
+
+instance : Zero (MultiCover F c) where
+  zero := ⟨fun x ↦ 0, by
+    use Empty
+    simp only [Set.coe_setOf, Cardinal.mk_eq_zero, nonpos_iff_eq_zero, zero_le, implies_true,
+      exists_const]
+    use emptyFun _
+    funext x
+    rw [Eq.comm, ENat.card_eq_zero_iff_empty]
+    exact instIsEmptySubtype fun n ↦ x ∈ emptyFun F n
+    ⟩
+
+instance : AddCommMonoid (MultiCover F c) where
+  zero_add a := by
+    rw [show 0 + a = ⟨0 + a,_⟩ by rfl]
+    simp only [zero_add]
+    rfl
+  add_zero a := by
+    rw [show a + 0 = ⟨a + 0,_⟩ by rfl]
+    simp only [add_zero]
+    rfl
+  nsmul := nsmulRec
+
+
+theorem MultiCover.add_fun_coe {a b : MultiCover F c} : ⇑(a + b) = ⇑a + ⇑b := by rfl
+theorem MultiCover.zero_fun_coe : ⇑(0 : MultiCover F c) = 0 := by rfl
+
+
+
+instance : LE (MultiCover F c) where
+  le a b := (a : X → ℕ∞) ≤ (b : X → ℕ∞)
+  -- le a b := LE.le (a : X → ℕ∞) (b : X → ℕ∞)
+
+instance : Preorder (MultiCover F c) where
+  le_refl a := by
+    intro x
+    exact Std.IsPreorder.le_refl _
+  le_trans := by
+    intro a b c ab bc x
+    exact Std.IsPreorder.le_trans (a x) (b x) (c x) (ab x) (bc x)
+
+instance : TopologicalSpace (MultiCover F c) := Preorder.topology _
+local instance : TopologicalSpace ℕ∞ := Preorder.topology _
+
+example : ℕ ≃ ℕ × ℕ := by exact Nat.pairEquiv.symm
+
+
+-- issue: the result of the sum has a different type...
+-- I should just let the cardinality be a property.
+
+theorem MultiCover.hasSum {ι : Type} (s : ι → MultiCover F c) : HasSum s ⟨
+  (∑' n, ⇑(s n)),
+  by
+  -- let series n : F := Nat.pairEquiv
+
+
+
+  sorry
+⟩ := by sorry
+
+theorem MultiCover.summable {ι : Type}  (s : ι → MultiCover F c) : Summable s := by sorry
+
+theorem MultiCover.sum_coe {ι : Type}  (s : ι → MultiCover F c) : (∑' n, ⇑(s n)) = ⇑(∑' n, (s n)) := by
+
+  sorry
+
+theorem MultiCover.sum {ι : Type}  (s : ι → MultiCover F c) (x : X) : (∑' n, (s n)) x = (∑' n, (s n x)) := by
+  -- rw [tsum]
+  -- apply ENNReal.tsum_apply
+  -- apply tsum_apply
+  -- rw [tsum_apply]
+  -- simp only [SummationFilter.support_eq_univ, Set.inter_univ, Set.indicator_univ]
+  sorry
+
+section exact_multi_cover
+
+structure ExactMultiCover {X : Type} (F : Type) [SetLike F X] where
+  func (x : X) : ℕ∞
+  possible: ∃(series: ℕ → F), func = fun x ↦ ENat.card {n | x ∈ series n}
+
+
+instance : FunLike (ExactMultiCover F) X ℕ∞ where
+  coe := ExactMultiCover.func
+  coe_injective' := by
+    intro a b fu
+    change ⟨a.func,_⟩ = b
+    simp only [fu]
+
+instance : Membership X (ExactMultiCover F) where
+  mem f x := f x > 0
+
+
+instance : Add (ExactMultiCover F) where
   add a b := ⟨(a + b), by
     have ⟨as,as_spec⟩ := a.possible
     have ⟨bs,bs_spec⟩ := b.possible
@@ -233,7 +420,8 @@ instance : Add (MultiCover F) where
     apply Equiv.subtypeSum
   ⟩
 
-instance : AddCommSemigroup (MultiCover F)  where
+
+instance : AddCommSemigroup (ExactMultiCover F)  where
   add_assoc a b c := by
     change a + b + c = ⟨a + (b + c), _⟩
     ring_nf
@@ -241,15 +429,11 @@ instance : AddCommSemigroup (MultiCover F)  where
   add_comm a b := by
     change ⟨a + b, _⟩ = b + a
     rw [show b + a = ⟨b + a, _⟩ by rfl]
-    simp only [MultiCover.mk.injEq]
+    simp only [ExactMultiCover.mk.injEq]
     exact AddCommMagma.add_comm ⇑a ⇑b
 
--- Surely this already exists.
-class HasEmpty (A : Type*) {B : outParam Type*} [SetLike A B] extends EmptyCollection A where
-  empty_is_empty : SetLike.coe (∅ : A) = ∅
 
-
-instance [HasEmpty F] : Zero (MultiCover F) where
+instance [HasEmpty F] : Zero (ExactMultiCover F) where
   zero := ⟨fun x ↦ 0, by
     use fun i ↦ ∅
     funext x
@@ -262,7 +446,9 @@ instance [HasEmpty F] : Zero (MultiCover F) where
     exact Subtype.isEmpty_of_false fun a ↦ this
     ⟩
 
-instance [HasEmpty F] : AddCommMonoid (MultiCover F) where
+
+
+instance [HasEmpty F] : AddCommMonoid (ExactMultiCover F) where
   zero_add a := by
     rw [show 0 + a = ⟨0 + a,_⟩ by rfl]
     simp only [zero_add]
@@ -274,16 +460,16 @@ instance [HasEmpty F] : AddCommMonoid (MultiCover F) where
   nsmul := nsmulRec
 
 
-theorem MultiCover.add_fun_coe {a b : MultiCover F} : ⇑(a + b) = ⇑a + ⇑b := by rfl
-theorem MultiCover.zero_fun_coe [HasEmpty F] : ⇑(0 : MultiCover F) = 0 := by rfl
+theorem ExactMultiCover.add_fun_coe {a b : ExactMultiCover F} : ⇑(a + b) = ⇑a + ⇑b := by rfl
+theorem ExactMultiCover.zero_fun_coe [HasEmpty F] : ⇑(0 : ExactMultiCover F) = 0 := by rfl
 
 
 
-instance : LE (MultiCover F) where
+instance : LE (ExactMultiCover F) where
   le a b := (a : X → ℕ∞) ≤ (b : X → ℕ∞)
   -- le a b := LE.le (a : X → ℕ∞) (b : X → ℕ∞)
 
-instance : Preorder (MultiCover F) where
+instance : Preorder (ExactMultiCover F) where
   le_refl a := by
     intro x
     exact Std.IsPreorder.le_refl _
@@ -291,56 +477,8 @@ instance : Preorder (MultiCover F) where
     intro a b c ab bc x
     exact Std.IsPreorder.le_trans (a x) (b x) (c x) (ab x) (bc x)
 
-instance : TopologicalSpace (MultiCover F) := Preorder.topology _
-local instance : TopologicalSpace ℕ∞ := Preorder.topology _
 
-example : ℕ ≃ ℕ × ℕ := by exact Nat.pairEquiv.symm
+end exact_multi_cover
 
-
-theorem MultiCover.hasSum [HasEmpty F]  (s : ℕ → MultiCover F) : HasSum s ⟨
-  (∑' n, ⇑(s n)),
-  by
-  -- let series n : F := Nat.pairEquiv
-
-
-
-  sorry
-⟩ := by sorry
-
-theorem MultiCover.summable [HasEmpty F]  (s : ℕ → MultiCover F) : Summable s := by sorry
-
-theorem MultiCover.sum_coe [HasEmpty F]  (s : ℕ → MultiCover F) : (∑' n, ⇑(s n)) = ⇑(∑' n, (s n)) := by
-
-  sorry
-
-theorem MultiCover.sum [HasEmpty F]  (s : ℕ → MultiCover F) (x : X) : (∑' n, (s n)) x = (∑' n, (s n x)) := by
-  -- rw [tsum]
-  -- apply ENNReal.tsum_apply
-  -- apply tsum_apply
-  -- rw [tsum_apply]
-  -- simp only [SummationFilter.support_eq_univ, Set.inter_univ, Set.indicator_univ]
-  sorry
-
-/-
-section test_structure_parameters
-
--- is test X = test Y if they don't use the parameter anywhere?
-
-structure test (X : Type) where
-  foo : Prop
-
-theorem test_heq {X Y : Type} : test X = test Y := sorry
-
-def test_back {X : Type} (t : test X) : Type := X
-
--- if this is true, I'm wrong.
-example {X Y : Type} (x : test X) (y : test Y) (h : test X = test Y) : X = Y := by
-  change test_back x = test_back y
-  rw [h] at x
-  -- x is now a different variable.
-  -- this is useless
-  sorry
-end test_structure_parameters
--/
 
 end multi_cover

0972922b2620a5a1b7f9190e431a33501a9f05b4
okkokko10 <okko.heinio@gmail.com>
2026-03-02 19:16:17 +0200

    changed syndrome_map, added 𝓛.dualLattice.mem_def', removed trivial implicits

--- Thesis/ThesisTry.lean
+++ Thesis/ThesisTry.lean
@@ -40,6 +40,17 @@ def dualLattice_basic : AddSubgroup (ι → ℝ) where
 
 def 𝓛.dualLattice : 𝓛 ι := (dualLattice_basic Λ).toIntSubmodule
 
+
+lemma 𝓛.dualLattice.mem_def' (x : ι → ℝ) :
+  x ∈ (dualLattice Λ) ↔
+  ∀ v ∈ Λ,  x ∈ (AddSubgroup.zmultiples (1 : ℝ)).toIntSubmodule.comap (dotProductBilin ℤ ℤ v) := by
+    unfold dualLattice dualLattice_basic
+    simp only [Int.range_castAddHom, Submodule.mem_comap, dotProductBilin_apply_apply,
+      dotProduct_comm]
+    rfl
+
+
+
 theorem 𝓛.dualLattice.involution : Function.Involutive (𝓛.dualLattice (ι := ι)) := sorry
 
 -- #check ZSpan
@@ -411,16 +422,16 @@ example (q)  [NeZero q] : Algebra ℤ (ZMod q) := inferInstance
 
 def A_Matrix.syndrome_map {n m q : ℕ} (A : A_Matrix n m q) : (Fin m → ℤ) →ₗ[ℤ] (Fin n → ZMod q) := by
   -- have := Matrix.toLin (m := Fin n) (n := Fin m) (R := ZMod q) sorry sorry
-  let vl:= Matrix.mulVecLin A
+  let vl:= (Matrix.mulVecLin A).toAddMonoidHom.toIntLinearMap
 
   let toZModLin (q) : ℤ →ₗ[ℤ] (ZMod q) := Algebra.linearMap ℤ (ZMod q)
   -- have this be →ₗ[ℤ] as well
   -- is converting to ZMod q the same before or after "this"?
   let : (Fin m → ℤ) →ₗ[ℤ] (Fin m → ZMod q) := by
     exact (toZModLin q).compLeft (Fin m)
-  exact Fintype.linearCombination ℤ fun a a_1 ↦ A a_1 a
 
-  -- refine ((LinearMap.comp this vl) )
+
+  refine vl.comp this
 
 
 -- this shows that modulo can be done before or after
@@ -431,28 +442,43 @@ def A_Matrix.syndrome_map' {n m q : ℕ} (A : A_Matrix n m q) : (Fin m → ℤ) 
   intro x
   apply A.mulVec <| Int.cast ∘ x
 
+
+theorem A_Matrix.syndrome_map_linearCombination {n m q : ℕ} (A : A_Matrix n m q) (x) :
+  A.syndrome_map x = (Fintype.linearCombination ℤ fun a a_1 ↦ A a_1 a) x := by
+  unfold syndrome_map
+
+  simp only [LinearMap.coe_comp, AddMonoidHom.coe_toIntLinearMap, LinearMap.toAddMonoidHom_coe,
+    Function.comp_apply, Matrix.mulVecBilin_apply]
+  ext i
+  simp [Fintype.linearCombination_apply ]
+  simp [Matrix.mulVec, dotProduct]
+  congr 1
+  ext j
+  exact Eq.symm (Int.cast_comm (x j) (A i j))
+
+
 section testing
-open Plausible
+-- open Plausible
 
 
 
-instance {q} : Arbitrary (ZMod q) :=
-  match q with
-    | 0 => Int.Arbitrary
-    | _ + 1 => Fin.Arbitrary
-instance {q} : Shrinkable (ZMod q) :=
-  match q with
-    | 0 => Int.shrinkable
-    | _ + 1 => Fin.shrinkable
-#test ∀i : (ZMod 5), i + 0 = i
-#test ∀i : (Fin 2 → Fin 2), i + 0 = i
+-- instance {q} : Arbitrary (ZMod q) :=
+--   match q with
+--     | 0 => Int.Arbitrary
+--     | _ + 1 => Fin.Arbitrary
+-- instance {q} : Shrinkable (ZMod q) :=
+--   match q with
+--     | 0 => Int.shrinkable
+--     | _ + 1 => Fin.shrinkable
+-- #test ∀i : (ZMod 5), i + 0 = i
+-- #test ∀i : (Fin 2 → Fin 2), i + 0 = i
 
--- experimentally checks that syndrome_map is correct
-#eval Testable.check
-    (∀ ee : _ → _ → (ZMod _),
-    let A : A_Matrix 3 4 5 := Matrix.of ee;
-    ∀xx, A.syndrome_map xx = A.syndrome_map' xx)
-  {traceSuccesses := true}
+-- -- experimentally checks that syndrome_map is correct
+-- #eval Testable.check
+--     (∀ ee : _ → _ → (ZMod _),
+--     let A : A_Matrix 3 4 5 := Matrix.of ee;
+--     ∀xx, A.syndrome_map xx = A.syndrome_map' xx)
+--   {traceSuccesses := true}
 
 
 
@@ -503,7 +529,9 @@ def A_Matrix.Λ_main {n m q : ℕ} [NeZero q] (A : A_Matrix n m q) : AddSubgroup
   := (A_Matrix.syndrome_map A.transpose).toAddMonoidHom.range.comap
   ((Int.castAddHom (ZMod q)).compLeft (Fin m))
 
-def to_R {m} (L : AddSubgroup (Fin m → ℤ) ) : 𝓛 (Fin m) := (AddSubgroup.map ((Int.castAddHom ℝ).compLeft (Fin m)) L).toIntSubmodule
+def to_R {m} (L : AddSubgroup (Fin m → ℤ) ) : 𝓛 (Fin m) := (L.map ((Int.castAddHom ℝ).compLeft (Fin m))).toIntSubmodule
+
+
 
 def A_Matrix.Λ_ortho' {n m q : ℕ} [NeZero q] (A : A_Matrix n m q) : 𝓛 (Fin m) := to_R A.Λ_ortho
 def A_Matrix.Λ_main' {n m q : ℕ} [NeZero q] (A : A_Matrix n m q) : 𝓛 (Fin m) := to_R A.Λ_main
@@ -527,10 +555,9 @@ lemma A_Matrix.Λ_ortho'.has_qZn {n m q : ℕ} [NeZero q] (A : A_Matrix n m q) :
     use Pi.single i q
     constructor
     {
-      ext j
-      unfold syndrome_map
-      simp only [Fintype.linearCombination_apply_single, Pi.smul_apply, zsmul_eq_mul,
-        Int.cast_natCast, CharP.cast_eq_zero, zero_mul, Pi.zero_apply]
+      ext jacobiSum
+      simp only [syndrome_map_linearCombination, Fintype.linearCombination_apply_single,
+        Pi.smul_apply, zsmul_eq_mul, Int.cast_natCast, CharP.cast_eq_zero, zero_mul, Pi.zero_apply]
     }
     ext j
     simp only [AddMonoidHom.compLeft_apply, Int.coe_castAddHom, Function.comp_apply]
@@ -562,14 +589,14 @@ def A_Matrix.syndrome_distributed {n m q : ℕ} [NeZero q] (A : A_Matrix n m q)
 end A_Matrix
 
 theorem lemma_5_2 {n m q : ℕ} [NeZero q] (A : A_Matrix n m q) (ass : lemma_5_1_statement A)
-  (ε : ℝ≥0) [NeZero ε] (ε_bound : ε < 2⁻¹) (s : ℝ≥0) [Fintype (Fin m)]
+  (ε : ℝ≥0) [NeZero ε] (ε_bound : ε < 2⁻¹) (s : ℝ≥0)
   (s_prop : s ≥ 𝓛.smoothing_parameter (A.Λ_ortho') ε) :
   let hs : NeZero s := sorry;
   statistical_distance (A.syndrome_distributed (int_gaussian m s)) (uniform_over_Zqn _ _) ≤ 2 * ε
   := sorry
 
 theorem lemma_5_2_furthermore {n m q : ℕ} [NeZero q] (A : A_Matrix n m q) (ass : lemma_5_1_statement A)
-  (ε : ℝ≥0) [NeZero ε] (ε_bound : ε < 2⁻¹) (s : ℝ≥0) [Fintype (Fin m)]
+  (ε : ℝ≥0) [NeZero ε] (ε_bound : ε < 2⁻¹) (s : ℝ≥0)
   (s_prop : s ≥ 𝓛.smoothing_parameter (A.Λ_ortho') ε) (u : Fin n → ZMod q) (t : Fin m → ℤ) (ht : A.syndrome_map t = u)
   :
   let hs : NeZero s := sorry;

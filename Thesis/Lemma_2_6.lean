9bf83b44afde0bda14f4ae9478a1233d5caab648
okkokko10 <okko.heinio@gmail.com>
2026-03-07 03:31:19 +0200

    progressed Lemma_2_6

--- Thesis/Lemma_2_6.lean
+++ Thesis/Lemma_2_6.lean
@@ -98,6 +98,120 @@ theorem Lemma_2_6_then''
       apply lemma_2_6_upper.mono _ _ ?_ (m n)
       apply negligible.smaller_le ε
 
+    suffices ∀n, ∃ ε : ℝ≥0, ∃ (ε_pos :  NeZero (ε)), lemma_2_6_upper (ε) (m n) ≤ s n by
+
+      refine ⟨fun n ↦ (this n).choose, ?_, ?_⟩
+      exact fun n ↦ (this n).choose_spec.choose
+      exact fun n ↦ (this n).choose_spec.choose_spec
+    intro n
+
+    -- the rest is clear
+
+    let m' := m n
+
+
+    have : (fun ε : ℝ≥0 ↦ ((1 + ε⁻¹)⁻¹).toReal) = fun ε ↦ ((1 + (ε.toReal)⁻¹)⁻¹) := by rfl
+
+    set d : ℝ≥0 → _ :=
+      (Real.toNNReal) ∘ (√·) ∘ (· / Real.pi) ∘
+      (Real.log) ∘ ((2 * m') * ·) ∘
+      (·⁻¹) ∘ (1 + ·) ∘ (·⁻¹)  ∘ NNReal.toReal
+
+    change ∃ε, ∃ε_pos, d ε ≤ s n
+
+    let w := (√(Real.log (2 * m') / Real.pi)).toNNReal / 2
+
+
+    set s' := s n ⊓ w
+    suffices (s') ∈ d '' (Set.Ioi 0) by
+      simp only [Set.mem_image, Set.mem_Ioi] at this
+      obtain ⟨x, pos_x, dxs⟩ := this
+      refine ⟨x,NeZero.of_pos pos_x,?_⟩
+      rw [dxs]
+      subst s'
+      exact min_le_left (s n) w
+    subst d
+    convert_to
+      s' ∈
+        Real.toNNReal '' (
+        (√·) ''
+        ((· / Real.pi) ''
+        (Real.log ''
+        (((2 * ↑m') * ·) ''
+        ((·⁻¹) ''
+        ((1 + ·) ''
+        ((·⁻¹) ''
+        (NNReal.toReal '' Set.Ioi 0))))))))
+    · simp only [Set.image_comp]
+
+    set uu := 2 * (m' : ℝ)
+    have uu_pos : 0 < uu := by
+      subst uu
+      simp only [Nat.ofNat_pos, mul_pos_iff_of_pos_left, Nat.cast_pos]
+      exact Nat.pos_of_neZero m'
+
+    have : (NNReal.toReal '' Set.Ioi 0) = Set.Ioi 0 := by
+      ext x
+      simp only [Set.mem_image, Set.mem_Ioi]
+      refine ⟨fun ⟨y,y_pos, yx⟩ ↦ ?_,fun x_pos ↦ ⟨⟨x,x_pos.le⟩,x_pos,rfl⟩⟩
+      exact lt_of_lt_of_eq y_pos yx
+    rw [this]
+
+    have : (fun ε : ℝ ↦ (ε⁻¹)) '' Set.Ioi 0 = Set.Ioi 0 := by
+      ext x
+      simp only [Set.image_inv_eq_inv, Set.mem_inv, Set.mem_Ioi, inv_pos]
+    rw [this]
+    have : (fun ε : ℝ ↦ 1 + ε) '' Set.Ioi 0 = Set.Ioi 1 := by
+      ext x
+      simp only [Set.image_add_left, Set.preimage_const_add_Ioi, sub_neg_eq_add, zero_add,
+        Set.mem_Ioi]
+
+    rw [this]
+    have : (fun ε : ℝ ↦ (ε⁻¹)) '' Set.Ioi 1 = Set.Ioo 0 1 := by
+      ext x
+      simp only [Set.image_inv_eq_inv, Set.mem_inv, Set.mem_Ioi, Set.mem_Ioo]
+      apply one_lt_inv_iff₀
+    rw [this]
+    have : ((fun x : ℝ ↦ uu * x) '' Set.Ioo 0 1) = Set.Ioo 0 (uu) := by
+      ext y
+      simp only [Set.mem_image, Set.mem_Ioo]
+
+      refine ⟨fun ⟨x,⟨x0,x1⟩,xy⟩ ↦ ?_,?_⟩
+      rw [←xy]
+      constructor
+      apply mul_pos uu_pos x0
+      exact mul_lt_of_lt_one_right uu_pos x1
+      intro ⟨y_pos,y_uu⟩
+      use y * uu⁻¹
+      refine ⟨⟨?_,?_⟩,?_⟩
+      field_simp
+      ring_nf
+      exact y_pos
+      field_simp
+      exact y_uu
+      field_simp
+
+    rw [this]
+    have : (Real.log '' Set.Ioo 0 (uu)) = Set.Iio (Real.log (uu)) := by
+      ext x
+      simp
+
+
+      sorry
+    rw [this]
+    have : ((fun x ↦ x / Real.pi) '' Set.Iio (Real.log (2 * ↑m'))) =  Set.Iio (Real.log (2 * ↑m') / Real.pi) := by sorry
+    rw [this]
+    have : ((fun x ↦ √x) '' Set.Iio (Real.log (2 * ↑m') / Real.pi)) = Set.Ico 0 (√(Real.log (2 * ↑m') / Real.pi)) := by
+      sorry
+    rw [this]
+    have : (Real.toNNReal '' Set.Ico 0 (√(Real.log (2 * ↑m') / Real.pi))) = Set.Ico 0 (Real.toNNReal √(Real.log (2 * ↑m') / Real.pi))  := by sorry
+    rw [this]
+    simp only [Set.mem_Ico, zero_le, true_and, gt_iff_lt]
+    subst s'
+    simp
+    right
+    subst w
+    simp only [half_lt_self_iff, Real.toNNReal_pos, Real.sqrt_pos]
 
 
 
